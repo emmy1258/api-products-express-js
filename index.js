639a220e3afdee4544c780fe3bc4433099148b1b
imane chater <imane.chater30@gmail.com>
2023-10-02 01:53:45 -0700

    mongodb setup added

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const { MongoClient } = require('mongodb');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+
 const express = require('express');
 
 const app = express();
 
 const PORT = 3000;
+
+// Connect to the server
+client.connect((err) => {
+    if (err) {
+      console.error('Failed to connect to the database:', err);
+      return;
+    }
+    console.log('Connected to the database');
+    
+    // Perform database operations here
+    
+    // Close the connection
+    client.close();
+  });
+  
 
 const products = [
     {
