811c972a79405d877e6c3cdfa8bff7622c30b371
imane chater <imane.chater30@gmail.com>
2023-10-08 08:23:14 -0700

    new changes

--- routes/products.route.js
+++ routes/products.route.js
@@ -6,17 +6,13 @@ const router = express.Router();
 const productController = require('../controllers/products.controller')
 
 
-//get all products
-router.get('/api/products', productController.getAllProducts)
+router.route('/')
+    .get(productController.getAllProducts)
+    .post(productController.addProduct)
 
-//get a single product
-router.get('/api/courses/:courseID', productController.getProduct)
+router.route('/api/courses/:courseID')
+    .get(productController.getProduct)
+    .patch(productController.updateProduct)
+    .delete(productController.deleteProduct)
 
-//create a new course
-router.post('/api/products',productController.addProduct)
-
-// change a product
-router.patch('/api/products/:productID', productController.updateProduct)
- 
-//delete a product
-router.delete('api/products/:productID', productController.deleteProduct)
+ module.exports = router;
